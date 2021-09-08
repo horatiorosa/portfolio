0176af885c6497a3b8805b3637fed63dffcecdb9
horatio <horatiorosa@aol.com>
2021-09-08 17:16:56 -0400

    some neat ES6 stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
-console.log('scripts loaded');
+const wait = (ms = 0) => new Promise(resolve => setTimeout(resolve, ms));
+const app = document.querySelector('.app');
+
+async function animateTitle() {
+  console.log('starting');
+  const titleSpan = app.querySelector('.title');
+
+  const titles = ['Dog Dad', 'Engineer', 'Volleyball Player', 'Human', 'Web Developer'];
+  for (const title of titles) {
+    await wait(1500);
+    const newTitle = await title;
+    titleSpan.innerText = newTitle;
+    console.log(newTitle);
+  }
+
+  console.log('ending');
+}
+
+animateTitle();
+
+  // on load, animate the title
+  // on hover / mouse enter, animate the tite
+  // change background image to match the title
+  // at the end of each animation, title should be the default (web dev)
