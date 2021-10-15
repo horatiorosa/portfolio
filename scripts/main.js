6fc8ecab0365caa8b959f2024a38ed3286503454
horatio <horatiorosa@aol.com>
2021-10-15 17:10:50 -0400

    wired up contact form  - ripple not working yet  - still hitting close modal twice

--- scripts/main.js
+++ scripts/main.js
@@ -6,39 +6,59 @@ const modalOuter = body.querySelector('.modal__outer');
 const modalInner = modalOuter.querySelector('.modal__inner');
 const close = modalInner.querySelector('.modal__button-close');
 const rippleOrigin = modalOuter.querySelector('.ripple-origin');
+const contactLink = body.querySelector('.contact_form');
+// const contactForm = modalOuter.querySelector('.form_container');
+let pageLoadedCount = 0,
+  closeModalCounter = 0,
+  openModalCounter = 0,
+  animationCounter = 0;
+
 
 
 /* to do: clean up "under construction" related items when MVP is ready */
 function handlePageLoad() {
-  body.classList.add('hidden-overflow');
-  app.classList.add('background-blur');
-  modalOuter.classList.add('modal__outer-open', 'modal__outer-background');
+  pageLoadedCount += 1;
+  // console.log(`page loaded ${pageLoadedCount} times`);
 
-  modalOuter.addEventListener('click', handleClick);
-  close.addEventListener('click', handleClick);
+  openModal();
+
+  modalOuter.addEventListener('click', handleModalClick);
+  close.addEventListener('click', handleModalClick);
   window.addEventListener('keyup', handleKeyUp);
 
   document.removeEventListener('DOMContentLoaded', pageLoaded);
 }
 
-async function closeModal() {
-  modalCloseAnimation();
+const openModal = () => {
+  body.classList.add('hidden-overflow');
+  app.classList.add('background-blur');
+  modalOuter.classList.add('modal__outer-open', 'modal__outer-background');
+  modalInner.classList.add('modal__inner-open', 'modal__inner-construction');
 
-  // await wait(1500);
+
+  openModalCounter += 1;
+  // console.log(`opened modal ${openModalCounter} times`);
+}
+
+const closeModal = () => {
+  app.classList.remove('background-blur');
   body.classList.remove('hidden-overflow');
   rippleOrigin.classList.remove('ripple-origin-show', 'ripple');
   modalOuter.classList.remove('modal__outer-open', 'modal__outer-background');
-  app.classList.remove('background-blur');
+  modalInner.classList.remove('modal__inner-open', 'modal__inner-construction');
 
   window.scrollTo(0,0);
-  modalOuter.removeEventListener('click', handleClick);
-  close.removeEventListener('click', handleClick);
-  window.removeEventListener('keyup', handleKeyUp);
+  close.removeEventListener('click', handleModalClick);
 
-  animateTitle();
+  if (openModalCounter <= 1) {
+    animateTitle();
+  }
+
+  closeModalCounter += 1;
+  // console.log(`closed modal ${closeModalCounter} times`);
 }
 
-function handleClick(e) {
+function handleModalClick(e) {
   if ((e.currentTarget === close) ||
     (e.target === e.currentTarget)) {
     return closeModal();
@@ -51,15 +71,12 @@ function handleKeyUp(e) {
 
 async function expireModal() {
   await wait(3000);
-  if (modalOuter.classList.contains('open')) {
-    closeModal();
+  if (modalOuter.classList.contains('modal__outer-open')) {
+    // closeModal();
   }
 }
 
-function modalCloseAnimation() {
-  modalInner.style.cssText = `display: none;`;
-  // rippleOrigin.classList.add('ripple-origin-show', 'ripple');
-}
+
 
 // smooth scrolling
 const smoothScrolling = e => {
@@ -84,21 +101,19 @@ function handleLinkClick() {
   }
 }
 
-
 /* title animation */
 async function animateTitle() {
   const titleSpan = app.querySelector('.title');
   const backgroundImage = app.querySelector('.background-image');
 
   const titles = [
-    {'title': 'Web Developer', 'image': './images/sublime.jpg', 'alt': 'photo of Sublime Text editor with code'},
     {'title': 'Dog Dad', 'image': './images/horatio_jiro_family_portrait.jpg', 'alt': 'man and his dog posed in a family style photo'},
     {'title': 'Volleyball Player', 'image': './images/volleyball_team_lets_go.jpg', 'alt': 'men and women of a volleyball team in a huddle'},
     {'title': 'Web Developer', 'image': './images/sublime.jpg', 'alt': 'photo of Sublime Text editor with code'}
   ];
 
   for (const key of titles) {
-    await wait(500);
+    await wait(100);
     const titleText = await key.title;
     const imageSource= await key.image;
     const altText = await key.alt;
@@ -108,17 +123,15 @@ async function animateTitle() {
     titleSpan.classList.add('fade-in-text');
     backgroundImage.classList.add('fade-in');
     backgroundImage.alt = altText;
-    await wait(3000);
+    await wait(1500);
     titleSpan.classList.remove('fade-in-text');
     backgroundImage.classList.remove('fade-in');
-
   }
 };
 /* ----------------- */
 
 /* animate contact link */
-const startContactAnimation = e => {
-  const contactLink = document.querySelector('.contact_form');
+const contactAnimation = () => {
   const paperPlane = `
   <img src="images/paper_plane.svg" alt="clickable paper plame image for contact form" class="paper_plane">
   `;
@@ -134,35 +147,69 @@ const startContactAnimation = e => {
   });
 }
 
-
-
 // contact me
 const contactForm = `
   <div class="form_container">
     <h3>Contact Me!</h3>
     <form method="post" action="" name="contact_form">
-      <label for="full_name">Thine Name</label>
-      <input name="full_name" type="text" required placeholder="John" />
+      <label for="full_name">Your Name</label>
+      <input name="full_name" type="text"  placeholder="John" />
       <br>
-      <label for="email">Thine Email</label>
-      <input name="email" type="email" required placeholder="you@domain.com" />
+      <label for="email">Your Email</label>
+      <input name="email" type="email"  placeholder="you@domain.com" />
       <br>
-      <label for="message">Thine Message</label><br>
-      <textarea name="message" cols="30" rows="10" placeholder="Enter your message here ..." required> </textarea>
+      <label for="message">Your Missive</label><br>
+      <textarea name="message" cols="30" rows="10" placeholder="Enter your message here ..." > </textarea>
       <div class="center">
-        <input type="submit" value="Submit">
+        <input type="submit"
+          type="button"
+          value="Submit"
+          onclick="sendMessage();">
       </div>
     </form>
   </div>
 `;
 
+const openContactForm = () => {
+  openModal();
+  modalInner.classList.remove('modal__inner-construction')
+  modalInner.innerHTML = contactForm;
+}
+
+
+
+const sendMessage = () => {
+  animationCounter += 1;
+
+  // console.log('animationCounter', animationCounter);
+  if (animationCounter >= 2) {
+    rippleOrigin.classList.add('ripple-origin-show', 'ripple');
+  }
+
+  modalInner.innerHTML = '';
+  closeModal();
+}
+
+
+async function animateRipple() {
+  rippleOrigin.classList.add('ripple-origin-show', 'ripple');
+  // console.log('ripple');
+  await wait(1000);
+}
+
 // call functions & event listeners
-startContactAnimation();
+contactAnimation();
 handleLinkClick();
 expireModal();
 
 const pageLoaded = document.addEventListener('DOMContentLoaded', handlePageLoad);
-
+contactLink.addEventListener('click', openContactForm);
+modalOuter.addEventListener('click', handleModalClick);
+window.addEventListener('keyup', handleKeyUp);
+modalOuter.addEventListener('submit', function(e) {
+  e.preventDefault();
+  sendMessage();
+});
 
 // const select = (el, all = false) => {
 //   el = el.trim()
